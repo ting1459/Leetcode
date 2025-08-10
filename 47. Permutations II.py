5a811805483c09cc6104f05fcaac0eb275507bfb
ting1459 <129727662+ting1459@users.noreply.github.com>
2025-08-10 13:13:54 -0500

    Update 47. Permutations II.py

--- 47. Permutations II.py
+++ 47. Permutations II.py
@@ -9,28 +9,28 @@ class Solution:
         def backtrack(path):
             print("step1:Calling backtrack with path:", path)  # fixed indentation here
             if len(path) == len(nums):
-                print("step2:Base case reached with path:", path)
+                print("step7:Base case reached with path:", path,"return")
                 result.append(path[:])
                 return 
         
 
             for i in range(len(nums)):
                 if used[i]:
-                    print(f"step3.0:{nums[i],i} used, skip to next")
+                    print(f"step2.0:{nums[i],i} used, skip to next")
                     continue
                 if i > 0 and nums[i] == nums[i-1] and not used[i-1]:
-                    print("step3.1:previous not used, skip to next i")
+                    print("step2.1:previous not used, skip to next i")
                     continue
                 
-                print(f"step4:use {nums[i],i} ")
+                print(f"step3:use {nums[i],i} ")
                 used[i] = True
                 path.append(nums[i])
-                print(f"step5:{path} appended")
+                print(f"step4:{path} appended, backtrack({path})\n")
                 backtrack(path)
-                print(f"step6.0:out loop,path before: {path}")
+                print(f"step5.0:out backtrack(),path before: {path}")
                 path.pop()
-                print(f"step6.1:pop(),path after: {path}")
-                print(f"step7:{nums[i],i} change to not used")
+                print(f"step5.1:pop(),path after: {path}")
+                print(f"step6:{nums[i],i} change to not used")
                 used[i] = False
             
         backtrack([])
